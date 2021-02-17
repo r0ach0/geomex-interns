e9b8a9117de3942e325fc1b66f88e311b037abe7
DESKTOP-FRE5CFD\admin <wnsgur5232@gmail.com>
2021-02-17 14:52:52 +0900

    border컨트롤러 및 home.jsp 제작

--- src/main/java/geomex/controller/BorderController.java
+++ src/main/java/geomex/controller/BorderController.java
@@ -1,5 +1,9 @@
 package geomex.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,13 +13,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import geomex.impls.ArticleService;
 import geomex.models.*;
 
+
 @Controller
-@SessionAttributes({"article"})
+@SessionAttributes({ "article" })
 public class BorderController {
     
-    
-    
+    private static final String Home = "home/home";
+
+    Logger logger = LoggerFactory.getLogger(BorderController.class);
+
+    @Resource(name = "articleService")
+    private ArticleService articleService;
+
+    @RequestMapping(value = "/articleList.do", method = RequestMethod.GET)
+    public String articleView(@ModelAttribute("article") ArticleVO view, Model model) throws Exception {
+        logger.debug(">>>>>>>>>>>>>>>" + view.toString());
+
+        List<ArticleVO> articleVO = articleService.selectRecentArticles(10);
+        if(articleVO != null){
+            model.addAttribute("article",articleVO);
+            return Home;
+        }
+        return Home;       
+    }
 
 }
