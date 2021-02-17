839814bc6fffc388c2b62eb2b3bfadf918b52c35
DESKTOP-FRE5CFD\admin <wnsgur5232@gmail.com>
2021-02-17 14:12:16 +0900

    master merge

---
+++ src/main/java/geomex/controller/BorderController.java
@@ -0,0 +1,21 @@
+package geomex.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import geomex.models.*;
+
+@Controller
+@SessionAttributes({"article"})
+public class BorderController {
+    
+    
+    
+
+}
